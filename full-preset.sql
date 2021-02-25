15b22cb8de936cc18b5fbeb276114114ce461b16
twentythreetea <helimc.minecraft@gmail.com>
2021-02-25 21:42:35 +0100

    pog changes :)

--- full-preset.sql
+++ full-preset.sql
@@ -40,31 +40,46 @@ CREATE TABLE IF NOT EXISTS `user_sessions`(
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- DROP TABLE IF EXISTS `articles`;
-CREATE TABLE IF NOT EXISTS `articles`(
-	`article_id` int AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `pages`(
+	`page_id` int AUTO_INCREMENT,
 	`user_id` int,
-	`article_content_id` int, 
+    `page_type` varchar(32),
+	`page_content_id` int, 
 	`hidden` bool NOT NULL,
 	`created_at` datetime NOT NULL,
-	PRIMARY KEY(`article_id`),
-	UNIQUE KEY `article_id` (`article_id`),
+	PRIMARY KEY(`page_id`),
+	UNIQUE KEY `page_id` (`page_id`),
 	KEY `fk_user`(`user_id`),
     CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- DROP TABLE IF EXISTS `article_updates`;
-CREATE TABLE IF NOT EXISTS `article_content`(
-	`article_content_id` int AUTO_INCREMENT,
-    `article_id` int NOT NULL,
+CREATE TABLE IF NOT EXISTS `page_content`(
+	`page_content_id` int AUTO_INCREMENT,
+    `page_id` int NOT NULL,
+    `page_image_id` int,
 	`user_id` int,
 	`title` tinytext NOT NULL,
 	`content` text NOT NULL,
     `last_update` datetime NOT NULL, -- this is when the update was created / when the article was updated 
-	PRIMARY KEY(`article_content_id`),
-	UNIQUE KEY `article_content_id` (`article_content_id`),
+	PRIMARY KEY(`page_content_id`),
+	UNIQUE KEY `page_content_id` (`page_content_id`),
 	KEY `fk_user`(user_id),
-	KEY `fk_article`(article_id),
-    CONSTRAINT `fk_article` FOREIGN KEY (`article_id`) REFERENCES `articles`(`article_id`)
+	KEY `fk_page`(page_id),
+    KEY `fk_page_image`(page_image_id),
+	CONSTRAINT `fk_page_image` FOREIGN KEY (`page_image_id`) REFERENCES `page_image`(`page_image_id`),
+    CONSTRAINT `fk_page` FOREIGN KEY (`page_id`) REFERENCES `pages`(`page_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS `page_image` (
+	`page_image_id` int AUTO_INCREMENT,
+    `page_id` int NOT NULL,
+    `user_id` int,
+    `file_format` varchar(16) NOT NULL,
+    PRIMARY KEY(`page_image_id`),
+	UNIQUE KEY `page_image_id` (`page_image_id`),
+    KEY `fk_user`(user_id),
+	KEY `fk_page`(page_id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 SET @user_id = 1;
@@ -72,14 +87,15 @@ SET @title = "test";
 SET @content = "cotent";
 SET @last_update = UTC_TIMESTAMP();
 
-INSERT INTO `articles`(`user_id`, `hidden`, `created_at`)
+INSERT INTO `pages`(`user_id`, `page_type`,`hidden`, `created_at`)
 	VALUES(
 		(SELECT @user_id),
+        "artikel",
         false,
 		(SELECT @last_update) -- twice cause the created at is the same as last update whebn you crewate an article :3
 );
 
-INSERT INTO `article_content`(`article_id`, `user_id`, `title`, `content`, `last_update`) 
+INSERT INTO `page_content`(`page_id`, `user_id`, `title`, `content`, `last_update`) 
 	VALUES(
 		LAST_INSERT_ID(),
 		(SELECT @user_id),
