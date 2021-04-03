31ecb0cee2bdfbad42d9ea3e16a99df80f4d99c4
twentythreetea <helimc.minecraft@gmail.com>
2021-04-03 21:01:10 +0000

    rm old files

--- example-for-momo.sql
+++
@@ -1,4 +0,0 @@
-UPDATE ringo.users SET flags = 16 WHERE user_id = 1;	-- PATCH
-INSERT INTO ringo.users() VALUES();						-- POST
-DELETE FROM ringo.users WHERE user_id = 7; 				-- DELETE
-SELECT * FROM ringo.users;								-- GET
